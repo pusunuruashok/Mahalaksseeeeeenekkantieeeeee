648546c1326ab9766e1799463660f68a34ce6a2d
Ashok Pusunuru <pusunuruashok99@gmail.com>
2026-04-19 21:54:48 -0400

    we are hiding the details

---
+++ one.java
@@ -0,0 +1,8 @@
+public class one {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming");
+        System.out.println("This is a simple Java program in the system matching the profile in the system property");
+    }
+}
+
