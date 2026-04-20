7ac805a32e4b85b4fc30bba114647d0d162b7c91
Ashok Pusunuru <pusunuruashok99@gmail.com>
2026-04-19 22:14:50 -0400

    data

---
+++ Two.java
@@ -0,0 +1,10 @@
+public class Two {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("Welcome to Java programming");
+        System.out.println("This is a simple Java program in the system matching the profile in the system property");
+       System.out.println("This is a simple Java program in the system matching the profile in the system property");
+    }
+    
+}
+
